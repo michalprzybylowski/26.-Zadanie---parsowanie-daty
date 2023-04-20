d095a83f9e56612f7e97f523569fdbc11c3bb539
Michał Przybyłowski <myss1990@gmail.com>
2023-04-20 17:51:15 +0200

    Add project files.

---
+++ 26. Zadanie - parsowanie daty/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Parsowanie.Daty
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool Done = false;
+            do
+            {
+                DateTime DateNow = DateTime.Now;
+                Console.WriteLine("Podaj date urodzin(yyyy-mm-dd):");
+                string Dateofbirth_s = Console.ReadLine();
+                try
+                {
+                    DateTime Dateofbirth = Convert.ToDateTime(Dateofbirth_s);
+                    double TotalDays = (DateNow - Dateofbirth).TotalDays;
+                    Console.WriteLine(TotalDays.ToString("F"));
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Data urodzin musi być w formacie yyyy-mm-dd");
+                }
+            } while (true);
+
+
+        }
+    }
+}
